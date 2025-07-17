dbcef4de62cc088b013003e3cf4cc46a0f11fd89
Ritik Nagpure <80842165+Ritik-Nagpure@users.noreply.github.com>
2025-07-18 00:52:45 +0530

    initialization

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './Kiracar/',
+  // base: '/Kiracar/',
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
